cd07b94f4712fbd7a5147c72ca52d2fccbe8a31f
NicoMeyer <nmeyer@bbr.cl>
2021-04-29 15:29:43 -0400

    readjusting commit

--- chapter6/src/main/resources/db/stored-function.sql
+++
@@ -1,8 +0,0 @@
-CREATE FUNCTION getFirstNameById(in_id INT)
- RETURNS VARCHAR(60)
- declare 
- 	firstName varchar(60);
- BEGIN
- RETURN SELECT first_name into firstName FROM singer WHERE id = in_id);
-	return firstName;
- END 
