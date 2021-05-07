12816842ea7352e6f67e66042786d00e1918112c
NicoMeyer <nmeyer@bbr.cl>
2021-05-07 16:07:18 -0400

    "finished" ch8, entity ver. did't work

--- chapter8 - Copy/src/main/java/com/prospring/ch8/service/SingerSummaryService.java
+++
@@ -1,9 +0,0 @@
-package com.prospring.ch8.service;
-
-import java.util.List;
-
-import com.prospring.ch8.view.SingerSummary;
-
-public interface SingerSummaryService {
-	List<SingerSummary> findAll();
-}
--- chapter8/src/main/java/com/prospring/ch8/config/temp/DataJpaConfig.java
+++ chapter8/src/main/java/com/prospring/ch8/config/temp/DataJpaConfig.java
@@ -1,4 +1,4 @@
-package com.prospring.ch8.config;
+package com.prospring.ch8.config.temp;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
